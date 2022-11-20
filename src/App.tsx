6eb462e8a30f43516c84e58d65056fd9a850c858
raumdeuter69 <samsixtynine69@gmail.com>
2022-11-20 23:49:22 +0530

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import SeasonsDisplay from './Components/SeasonsDisplay';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
