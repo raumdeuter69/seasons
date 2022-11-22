95e6e830e214ac1bb1265c1b30b4c94105c304d1
raumdeuter69 <samsixtynine69@gmail.com>
2022-11-23 03:51:59 +0530

    Refactored App.tsx to Class Based Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { FC } from 'react';
 import './App.css';
 import SeasonsDisplay from './Components/SeasonsDisplay';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+// function App() {
+//   window.navigator.geolocation.getCurrentPosition((position)=>(console.log(position)),
+//   (err)=>(console.log(err)));
+//   return (
+//         <div className="App">
+//           <h1>Hellloo</h1>
+//     </div>
+//   );
+// }
+interface props {
+  children?: React.ReactNode | React.ReactNode[];
+  }
+class App extends React.Component<{},{latitude :number | null}>
+{
+  constructor (props: {} | Readonly<{}>) {
+// Constructor of React.Component
+    super(props);
+// Only time we do direct assignment to this.state
+this.state={latitude:null};                               
+
+// API Call
+window.navigator.geolocation.getCurrentPosition((position)=>{
+  this.setState({latitude:position.coords.latitude})
+},
+(err)=>(console.log(err)));
+}
+
+     render (){
+    return <h1>Hellooo: {this.state.latitude}</h1>
+  }
 }
 
 export default App;
